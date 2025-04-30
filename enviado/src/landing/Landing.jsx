4d389b8d7ab39264879fcad565864282a39d75ba
unknown <emmanueljoseph1320@gmail.com>
2025-04-30 10:40:16 +0100

    added media queries

--- enviado/src/landing/Landing.jsx
+++ enviado/src/landing/Landing.jsx
@@ -1,5 +1,7 @@
 import style from "../landing/landing.module.css";
 import Navbar from "./navbar/Navbar";
+import arrowImg from "../assets/Arrow.png";
+import ButtonImg from "../assets/ButtonImage.png";
 
 const Landing = () => {
   return (
@@ -9,10 +11,27 @@ const Landing = () => {
         <div className={style.bigTextContainer}>
           <h1>Your 360º </h1>
           <h1> marketing</h1>
-          <h1>solution </h1>
+          <h1>
+            solution
+            <span className={style.startHereWrapper}>
+              <span className={style.startHere}>starts here</span>
+              <img className={style.arrowImage} src={arrowImg} alt="arrow" />
+            </span>
+          </h1>
+
+          <div className={style.arrowButtonWrapper}>
+            <img src={ButtonImg} alt="" />
+            <button className={style.ctaButton}>Let's talk</button>
+          </div>
         </div>
 
-        <button>Let's talk</button>
+        <div className={style.smallTextContainer}>
+          <p>
+            We don't just come up with ideas, products or experiences that push
+            boundaries and challenge the status quo – we also bring in the
+            results!
+          </p>
+        </div>
       </div>
     </section>
   );
--- enviado/src/landing/navbar/Navbar.jsx
+++ enviado/src/landing/navbar/Navbar.jsx
@@ -4,16 +4,18 @@ import EnviadoLogo from "../../assets/EnviadoLogo.png";
 const Navbar = () => {
   return (
     <nav className={style.nav}>
-      <div className={style.Navlogo}>
-        <img src={EnviadoLogo} alt="company logo" />
-      </div>
-      <div className={style.NavContent}>
-        <div className={style.NavItems}>
-          <p>Home</p>
-          <p>About</p>
-          <p>Service</p>
-          <p>Our works</p>
-          <p>Contact Us</p>
+      <div className={style.navWrapper}>
+        <div className={style.Navlogo}>
+          <img src={EnviadoLogo} alt="company logo" />
+        </div>
+        <div className={style.NavContent}>
+          <div className={style.NavItems}>
+            <p>Home</p>
+            <p>About</p>
+            <p>Service</p>
+            <p>Our works</p>
+            <p>Contact Us</p>
+          </div>
         </div>
       </div>
     </nav>
