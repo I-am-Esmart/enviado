54414f682b7afa2e2749b6112edcdd76e35827b8
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2025-05-03 00:03:04 +0100

    added mobile menue items view

--- enviado/src/landing/navbar/Navbar.jsx
+++ enviado/src/landing/navbar/Navbar.jsx
@@ -2,9 +2,12 @@ import style from "./navbar.module.css";
 import EnviadoLogo from "../../assets/EnviadoLogo.png";
 import { useEffect, useState } from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
+import { LiaTimesSolid } from "react-icons/lia";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false); // State to track if the navbar is scrolled
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track if the mobile menu is open
 
   useEffect(() => {
     const handleScroll = () => {
@@ -50,9 +53,43 @@ const Navbar = () => {
             <img src={EnviadoLogo} alt="company logo" />
           </div>
 
-          <div className={style.menuBar}>
+          <div className={style.menuBar} onClick={() => setIsMenuOpen(true)}>
             <p> Menu</p>
             <FaBarsStaggered />
+          </div>
+        </div>
+
+        <div
+          className={`${style.mobileMenu} ${
+            isMenuOpen ? style.mobileMenuOpen : ""
+          }`}
+        >
+          <div className={style.mobileMenuContainer}>
+            <div className={style.mobileMenuContainerTop}>
+              <p>Menu</p>
+              <div className={style.mobileMenuContainerTopRight}>
+                <button className={style.mobileMenuContainerTopBtnWrapper}>
+                  <p>Cancel</p>
+                  <LiaTimesSolid onClick={() => setIsMenuOpen(false)} />
+                </button>
+              </div>
+            </div>
+
+            <div className={style.mobileMenuMiddle}>
+              <div className={style.mobileMenuMiddleContainer}>
+                <ul>home</ul>
+                <ul>about us</ul>
+                <ul>Service</ul>
+                <ul>Our work</ul>
+                <ul>contact us</ul>
+              </div>
+            </div>
+
+            <div className={style.mobileMenuBottomContainer}>
+              <button className={style.mobileMenuBottomContainerBtnWrapper}>
+                <p> Send a Message </p> <FaArrowRightLong />
+              </button>
+            </div>
           </div>
         </div>
       </nav>
