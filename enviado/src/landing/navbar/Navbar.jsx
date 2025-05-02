77b9e9eb4307558551b58d848501c2de1903f56b
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2025-05-02 09:16:04 +0100

    add mobile responsive menu bar

--- enviado/src/landing/navbar/Navbar.jsx
+++ enviado/src/landing/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import style from "./navbar.module.css";
 import EnviadoLogo from "../../assets/EnviadoLogo.png";
 import { useEffect, useState } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false); // State to track if the navbar is scrolled
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,32 +16,47 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`${style.nav} ${scrolled ? style.scrolled : ""}`}>
-      <div className={style.navWrapper}>
-        <div className={style.Navlogo}>
-          <img src={EnviadoLogo} alt="company logo" />
-        </div>
-        <div className={style.NavContent}>
-          <div className={style.NavItems}>
-            <p>
-              <a href="#home">Home</a>
-            </p>
-            <p>
-              <a href="#about">About</a>
-            </p>
-            <p>
-              <a href="#service">Service</a>
-            </p>
-            <p>
-              <a href="#ourwork">Our works</a>
-            </p>
-            <p>
-              <a href="#contactus">Contact Us</a>
-            </p>
+    <>
+      <nav className={`${style.nav} ${scrolled ? style.scrolled : ""}`}>
+        <div className={style.navWrapper}>
+          <div className={style.Navlogo}>
+            <img src={EnviadoLogo} alt="company logo" />
+          </div>
+          <div className={style.NavContent}>
+            <div className={style.NavItems}>
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#about">About</a>
+              </p>
+              <p>
+                <a href="#service">Service</a>
+              </p>
+              <p>
+                <a href="#ourwork">Our works</a>
+              </p>
+              <p>
+                <a href="#contactus">Contact Us</a>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <nav className={style.navMobile}>
+        <div className={style.navWrapperMobile}>
+          <div className={style.NavlogoMobile}>
+            <img src={EnviadoLogo} alt="company logo" />
+          </div>
+
+          <div className={style.menuBar}>
+            <p> Menu</p>
+            <FaBarsStaggered />
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
